c329e54842cfea03da58fe5187f2b65bd699c3da
skylerlee <skyler.ac.lee@gmail.com>
2025-04-02 00:03:07 +0800

    feat: add parser

---
+++ omega/parser.c
@@ -0,0 +1,8 @@
+// Copyright (C) 2020, Skyler. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#include "omega/parser.h"
+
+void omg_parser_init(omg_parser* parser) {}
+void omg_parser_free(omg_parser* parser) {}
---
+++ omega/parser.h
@@ -0,0 +1,14 @@
+// Copyright (C) 2020, Skyler. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#ifndef OMEGA_PARSER_H_
+#define OMEGA_PARSER_H_
+
+typedef struct omg_parser {
+} omg_parser;
+
+void omg_parser_init(omg_parser* parser);
+void omg_parser_free(omg_parser* parser);
+
+#endif  // OMEGA_PARSER_H_
