0e13531e9d1b074307bade31408862e591c48847
skylerlee <skyler.ac.lee@gmail.com>
2025-04-08 23:07:22 +0800

    feat: use re2c

---
+++ omega/scanner.h
@@ -0,0 +1,24 @@
+// Copyright (C) 2020, Skyler. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#ifndef OMEGA_SCANNER_H_
+#define OMEGA_SCANNER_H_
+
+typedef struct omg_scanner {
+  int pos;
+  int cur_line;
+  int cur_col;
+} omg_scanner;
+
+typedef struct omg_token {
+  int type;
+  int pos;
+  int lineno;
+  int colno;
+} omg_token;
+
+void omg_scanner_init(omg_scanner* scanner);
+omg_token omg_scanner_scan(omg_scanner* scanner);
+
+#endif  // OMEGA_SCANNER_H_
