c0ef23ae0c1acdaba2c9089391c3082e4d13f911
skylerlee <skyler.ac.lee@gmail.com>
2025-04-13 17:45:56 +0800

    feat: add unit test

---
+++ omega/io_util_test.cpp
@@ -0,0 +1,20 @@
+#include "tests/test_aux.h"
+
+__BEGIN_DECLS
+#include "omega/io_util.h"
+__END_DECLS
+
+#include <gtest/gtest.h>
+
+#ifndef OMG_TEST_DATA_PATH
+#define OMG_TEST_DATA_PATH "__undefined__"
+#endif  // OMG_TEST_DATA_PATH
+
+TEST(BasicTest, io_util_read) {
+  const char* input = OMG_TEST_DATA_PATH "/input.txt";
+  omg_string_buf buf = omg_string_buf_new();
+  omg_io_util_read_to_string_buf(input, &buf);
+  EXPECT_STREQ(buf.data, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n");
+  EXPECT_EQ(buf.size, 57);
+  omg_string_buf_free(buf);
+}
---
+++ tests/test_aux.h
@@ -0,0 +1,16 @@
+// Copyright (C) 2020, Skyler. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#ifndef TESTS_TEST_AUX_H_
+#define TESTS_TEST_AUX_H_
+
+#if defined(__cplusplus)
+#define __BEGIN_DECLS extern "C" {
+#define __END_DECLS }
+#else
+#define __BEGIN_DECLS
+#define __END_DECLS
+#endif
+
+#endif  // TESTS_TEST_AUX_H_
